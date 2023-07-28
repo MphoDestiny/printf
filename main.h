d34a5e2bef7e5331742df1e431ba731fb48ede75
MphoDestiny <mphomasegomo@gmail.com>
2023-07-27 22:52:43 -0700

    get_precision.c

--- main.h
+++ main.h
@@ -63,7 +63,7 @@ int print_octal(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 int print_hexadecimal(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
-int print_hexa_upper(va_lit types, char buffer[],
+int print_hexa_upper(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
 int print_hexa(va_list types, char map_to[],
